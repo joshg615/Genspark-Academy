ba3d093fab1ab8767d24467955137bc7c9678f30
Joshua Guzman <Joshua_guzman615@yahoo.com>
2022-01-28 07:57:56 -0500

    Uploading Dragon Cave and Guess my number project

---
+++ Guess The Number/src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+import java.util.Random;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Initializing variables
+        boolean test = true;
+        String name;
+        Scanner input = new Scanner(System.in);
+        String choice;
+
+        //Start of guessing loop
+        do{
+             //Start of program
+             System.out.println("Hello! What is your name?");
+             //Take Name Input
+             name = input.next();
+
+             //start guess statement
+             System.out.println("Well, " + name + " I am thinking of a number between 1 and 20 \n"
+             + "Take a guess.");
+
+             //gets a random number between 1-20
+             int cpuNum = new Random().nextInt((20) + 1);
+
+            //loop giving user six tries
+             for(int i = 1; i<=6; i++){
+                 int userChoice = input.nextInt();
+
+                 if(userChoice > cpuNum){
+                     System.out.println("Your number is too high \n" +
+                                        "Take a guess");
+                 }
+                 else if(userChoice < cpuNum){
+                     System.out.println("your number is too low \n" +
+                             "Take a guess.");
+                 }
+                 else if(userChoice == cpuNum) {
+                     System.out.println("Good job, " + name + " You guessed my number in " + i + " guesses");
+                     break;
+                 }
+             }
+             //asking if you want to play again
+            System.out.println("Would you like to play again? (y/n)");
+            choice =  input.next();
+
+         }while(choice.equals("y"));
+
+
+    }
+}
