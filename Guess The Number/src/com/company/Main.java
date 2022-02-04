a187bc306e3cbbc423871a32dee698ae1c203085
Joshua Guzman <Joshua_guzman615@yahoo.com>
2022-02-04 13:50:12 -0500

    Added Try/Catch on user input for name and number choice

--- Guess The Number/src/com/company/Main.java
+++ Guess The Number/src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 public class Main {
@@ -7,7 +8,8 @@ public class Main {
 	// write your code here
         //Initializing variables
         boolean test = true;
-        String name;
+        String name = "";
+        int userChoice = 0;
         Scanner input = new Scanner(System.in);
         String choice;
 
@@ -16,8 +18,11 @@ public class Main {
              //Start of program
              System.out.println("Hello! What is your name?");
              //Take Name Input
-             name = input.next();
-
+            try {
+                name = input.next();
+            }catch(InputMismatchException e){
+                System.out.println("That's not a name!");
+            }
              //start guess statement
              System.out.println("Well, " + name + " I am thinking of a number between 1 and 20 \n"
              + "Take a guess.");
@@ -26,22 +31,26 @@ public class Main {
              int cpuNum = new Random().nextInt((20) + 1);
 
             //loop giving user six tries
-             for(int i = 1; i<=6; i++){
-                 int userChoice = input.nextInt();
 
-                 if(userChoice > cpuNum){
-                     System.out.println("Your number is too high \n" +
-                                        "Take a guess");
-                 }
-                 else if(userChoice < cpuNum){
-                     System.out.println("your number is too low \n" +
-                             "Take a guess.");
-                 }
-                 else if(userChoice == cpuNum) {
-                     System.out.println("Good job, " + name + " You guessed my number in " + i + " guesses");
-                     break;
-                 }
-             }
+               for (int i = 1; i <= 6; i++) {
+                   try {
+                       userChoice = input.nextInt();
+                   }catch(InputMismatchException e){
+                       System.out.println("That's not a number!");
+                       break;
+                   }
+                   if (userChoice > cpuNum) {
+                       System.out.println("Your number is too high \n" +
+                               "Take a guess");
+                   } else if (userChoice < cpuNum) {
+                       System.out.println("your number is too low \n" +
+                               "Take a guess.");
+                   } else if (userChoice == cpuNum) {
+                       System.out.println("Good job, " + name + " You guessed my number in " + i + " guesses");
+                       break;
+                   }
+
+               }
              //asking if you want to play again
             System.out.println("Would you like to play again? (y/n)");
             choice =  input.next();
