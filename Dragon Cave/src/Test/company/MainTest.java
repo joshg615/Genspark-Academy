1c28ffec3bec1b2ed68946f22f6112e64b5120ac
Joshua Guzman <Joshua_guzman615@yahoo.com>
2022-02-08 21:25:15 -0500

    Added jUnit Testing

---
+++ Dragon Cave/src/Test/company/MainTest.java
@@ -0,0 +1,19 @@
+package company;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
--- Dragon Cave/src/com/company/Main.java
+++ Dragon Cave/src/com/company/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package company;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
