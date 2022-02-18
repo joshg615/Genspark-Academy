3b863a6b8bf505fdfc4a57a9a9713da26abd0da7
Joshua Guzman <Joshua_guzman615@yahoo.com>
2022-02-18 11:30:02 -0500

    Human vs Goblins
    Added constructors, stat variables, getters,  setters, overidden the toString method to write out what each object is and put in an attack method to human and goblin.

--- Humans Vs Goblins/src/com/HumanvsGoblins/Goblins.java
+++ Humans Vs Goblins/src/com/HumanvsGoblins/Goblins.java
@@ -1,4 +1,41 @@
 package com.HumanvsGoblins;
 
 public class Goblins {
+    private int health, strength;
+
+    public Goblins(){}
+    public Goblins(int a, int b){
+        setHealth(a);
+        setStrength(b);
+    }
+
+    public String toString(){
+        return "Goblin(Health = " + this.getHealth() + ", Strength = " + this.getStrength() + ")";
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+
+
+    public Human attack(Human human){
+        human.setHealth(human.getHealth() - this.getStrength());
+        return human;
+    }
+
+
 }
+
--- Humans Vs Goblins/src/com/HumanvsGoblins/Human.java
+++ Humans Vs Goblins/src/com/HumanvsGoblins/Human.java
@@ -1,4 +1,48 @@
 package com.HumanvsGoblins;
 
 public class Human {
+    private int health, strength, armor;
+
+    public Human(){}
+    public Human(int a, int b, int c){
+        setHealth(a);
+        setStrength(b);
+        setArmor(c);
+
+    }
+
+    public String toString(){
+        return "Human(Health = " + this.getHealth() + ", Strength = " + this.getStrength() + " Armor = " + this.getArmor() + ")";
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public Goblins attack(Goblins gob){
+        gob.setHealth(gob.getHealth() - this.getStrength());
+        return gob;
+
+    }
+
 }
