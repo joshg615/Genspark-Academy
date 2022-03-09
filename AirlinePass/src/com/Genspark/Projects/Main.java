39a5334f0c7bad0913bf596d79c1226b714155f4
Joshua Guzman <Joshua_guzman615@yahoo.com>
2022-03-09 10:28:18 -0500

    changes

--- AirlinePass/src/com/Genspark/Projects/Main.java
+++ AirlinePass/src/com/Genspark/Projects/Main.java
@@ -1,7 +1,6 @@
 package com.Genspark.Projects;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 
@@ -13,7 +12,6 @@ Boarding Pass Number, Date, Origin, Destination, Estimated time of arrival (ETA)
 Name, Email, Phone Number, Gender, Age
 
 Total Ticket Price*/
-<<<<<<< Updated upstream
 
 
 
@@ -24,25 +22,15 @@ Total Ticket Price*/
 
 
     public static void main(String[] args) {
-=======
-    public static void main(String[] args) throws IOException {
->>>>>>> Stashed changes
 	// write your code here
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
 
-<<<<<<< Updated upstream
 
         System.out.println("Hey lets book your trip today");
 
         System.out.println("Hey what's you name?");
 
-=======
-        System.out.println("hello");
-        System.out.println("heloo again");
-        System.out.println("5644");
-        reader.readLine();
->>>>>>> Stashed changes
     }
 }
